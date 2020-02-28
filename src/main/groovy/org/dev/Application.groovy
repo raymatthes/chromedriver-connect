8ba77b9679dbc78f7b0c39072cb29f4ce32ae751
Ray Matthes <ray.matthes@gmail.com>
2020-02-27 23:03:03 -0700

    support more platforms

--- src/main/groovy/org/dev/Application.groovy
+++ src/main/groovy/org/dev/Application.groovy
@@ -1,5 +1,6 @@
 package org.dev
 
+import org.apache.commons.lang3.SystemUtils
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
@@ -9,12 +10,15 @@ import org.openqa.selenium.chrome.ChromeOptions
  */
 class Application {
 
-    public static final String CHROMEDRIVER_PATH = './bin/macos/chromedriver'
+    public static final String CHROMEDRIVER_PATH_LINUX64 = './bin/linux64/chromedriver'
+    public static final String CHROMEDRIVER_PATH_MAC64 = './bin/mac64/chromedriver'
+    public static final String CHROMEDRIVER_PATH_WIN32 = './bin/win32/chromedriver.exe'
 
     static void main(String[] args) {
         println("howdy!")
 
-        System.setProperty('webdriver.chrome.driver', CHROMEDRIVER_PATH)
+        String path = determineExecutable()
+        System.setProperty('webdriver.chrome.driver', path)
         System.setProperty("webdriver.chrome.logfile", "chromedriver.log")
         System.setProperty("webdriver.chrome.verboseLogging", "true")
 
@@ -26,9 +30,19 @@ class Application {
 
         sleep(3000)
 
-        driver.quit()   
+        driver.quit()
 
         System.exit(0)
+    }
+
+    protected static String determineExecutable() {
+        String path = CHROMEDRIVER_PATH_MAC64
+        if (SystemUtils.IS_OS_LINUX) {
+            path = CHROMEDRIVER_PATH_LINUX64
+        } else if (SystemUtils.IS_OS_WINDOWS) {
+            path = CHROMEDRIVER_PATH_WIN32
+        }
+        path
     }
 
 }
